42e1569d986d116cc9882452d3e4922b52283864
ito.shion <frontier.engine@gmail.com>
2012-06-13 00:26:59 +0900

    The end of handson.

---
+++ Python-handson/module.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+# モジュール
+
+import math, random
+from datetime import date
+
+print math.pi
+print math.ceil(5.2)
+
+print random.random()
+
+print date.today()
+
+# see python.org/modindex.html
