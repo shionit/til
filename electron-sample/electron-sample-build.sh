4a1d9683bf0a1548ff192af9a3715972f8c9df93
shionit <frontier.engine@gmail.com>
2015-10-15 08:55:11 +0900

    first step: display "Hello World!".

---
+++ electron-sample/electron-sample-build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+electron-packager ./electron-sample electron-sample --platform=darwin,win32 --arch=x64 --version=0.33.8
+
---
+++ electron-sample/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#require node.js
+sudo npm -g install electron-prebuilt
+sudo npm i electron-packager -g
+
+
