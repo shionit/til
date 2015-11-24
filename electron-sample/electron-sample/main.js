7e7fdb93f0588fcd0da91a56e0c197e2822a2c8e
shionit <frontier.engine@gmail.com>
2015-11-25 03:09:43 +0900

    set Show/Hide toggle menu.

--- electron-sample/electron-sample/main.js
+++ electron-sample/electron-sample/main.js
@@ -4,15 +4,11 @@
 var app = require('app');
 // ウィンドウを作成するモジュール
 var BrowserWindow = require('browser-window');
-
 var Tray = require('tray');
 var Menu = require('menu');
 
 // クラッシュレポート
 require('crash-reporter').start();
-
-// メインウィンドウはGCされないようグローバル宣言
-var mainWindow = null;
 
 // 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function() {
@@ -21,28 +17,15 @@ app.on('window-all-closed', function() {
     }
 });
 
+var appIcon = null;
 // Electronの初期化完了後に実行
 app.on('ready', function() {
-    // メイン画面の表示。ウィンドウの幅、高さを指定できる
-    mainWindow = new BrowserWindow({
-        width: 500,
-        height:600,
-        transparent: true,
-        frame: false
-    });
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-    // ウィンドウが閉じられたらアプリも終了
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
-
     // メニューアイコン設定
-    var appIcon = new Tray(__dirname + '/images/icon.png');
+    appIcon = new Tray(__dirname + '/images/icon.png');
     // コンテキストメニュー追加
     var contextMenu = Menu.buildFromTemplate([
-        {label: '選択メニュー１', type: 'radio'},
-        {label: '選択メニュー２', type: 'radio'},
+        {label: 'ウィンドウを非表示', type: 'radio', click: function() { appIcon.window.hide(); }},
+        {label: 'ウィンドウを表示', type: 'radio', click: function() { appIcon.window.show(); }},
         {type: 'separator'},
         {label: 'サブメニュー', submenu: [
             {label: 'サブメニュー１'},
@@ -53,4 +36,22 @@ app.on('ready', function() {
     appIcon.setContextMenu(contextMenu);
     // アイコンにマウスオーバーした時の説明
     appIcon.setToolTip('This is sample.');
+
+    // メイン画面はデフォルト非表示。ウィンドウの幅、高さを指定できる
+    appIcon.window = new BrowserWindow({
+        width: 500,
+        height:500,
+        transparent: true,
+        frame: false,
+        show: false
+    });
+    // ドックも非表示
+    app.dock.hide();
+
+    appIcon.window.loadUrl('file://' + __dirname + '/index.html');
+
+    // ウィンドウが閉じられたらアプリも終了
+    appIcon.window.on('closed', function() {
+        appIcon.window = null;
+    });
 });
