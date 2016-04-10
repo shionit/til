6a1054601c024e9720ffc08053bd9e5b38f2e046
shionit <frontier.engine@gmail.com>
2016-04-11 08:38:08 +0900

    add option page.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,5 +1,6 @@
 chrome.browserAction.onClicked.addListener(function() {
+  var color = localStorage['color'] ? localStorage['color'] : 'red';
   chrome.tabs.executeScript(null, {
-    "code": "document.body.style.backgroundColor='green'"
+    "code": "document.body.style.backgroundColor='" + color + "'"
   });
 });
--- chrome-extension/myscript.js
+++ chrome-extension/myscript.js
@@ -1,13 +1,28 @@
-function changeColor(color) {
-  chrome.tabs.executeScript(null, {
-    "code":"document.body.style.backgroundColor='" + color + "'"
-    });
+var colors = document.getElementsByName('colors');
+
+document.getElementById('save').onclick = function() {
+  if (colors[0].checked) {
+    localStorage['color'] = 'red';
+  } else if (colors[1].checked) {
+    localStorage['color'] = 'green';
+  } else {
+    localStorage['color'] = 'blue';
+  }
 }
 
-document.getElementById('red').onclick = function() {
-  changeColor('red');
-}
-
-document.getElementById('yellow').onclick = function() {
-  changeColor('yellow');
+document.body.onload = function() {
+  switch(localStorage['color']) {
+    case 'red':
+      colors[0].checked = true;
+      break;
+    case 'green':
+      colors[1].checked = true;
+      break;
+    case 'blue':
+      colors[2].checked = true;
+      break;
+    default:
+      colors[0].checked = true;
+      break;
+  }
 }
