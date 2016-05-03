65bc8d8d9b3d859028a88342f5840b4b5dfc0098
shionit <frontier.engine@gmail.com>
2016-05-03 12:18:58 +0900

    update electron version

--- electron-sample/electron-sample-build.sh
+++ electron-sample/electron-sample-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-electron-packager ./src electron-sample --platform=darwin,win32 --arch=x64 --version=0.33.8 --overwrite
+electron-packager ./src electron-sample --platform=darwin,win32 --arch=x64 --version=0.37.8 --overwrite
 
