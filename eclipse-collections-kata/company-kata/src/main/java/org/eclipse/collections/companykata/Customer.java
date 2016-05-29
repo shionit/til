096efeb81554bedd7f56594cfa6136a18e3ff708
shionit <frontier.engine@gmail.com>
2016-05-29 23:24:00 +0900

    done Exercise5Test

--- eclipse-collections-kata/company-kata/src/main/java/org/eclipse/collections/companykata/Customer.java
+++ eclipse-collections-kata/company-kata/src/main/java/org/eclipse/collections/companykata/Customer.java
@@ -13,6 +13,7 @@ package org.eclipse.collections.companykata;
 import org.eclipse.collections.api.block.function.Function;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.impl.block.function.AddFunction;
+import org.eclipse.collections.impl.list.mutable.FastList;
 import org.eclipse.collections.impl.utility.ListIterate;
 import org.junit.Assert;
 
@@ -35,7 +36,7 @@ public class Customer
     private final String name;
     private final String city;
 
-    private final List<Order> orders = new ArrayList<>();
+    private final MutableList<Order> orders = new FastList<>();
 
     public Customer(String name, String city)
     {
@@ -53,7 +54,7 @@ public class Customer
         return this.name;
     }
 
-    public List<Order> getOrders()
+    public MutableList<Order> getOrders()
     {
         return this.orders;
     }
--- eclipse-collections-kata/company-kata/src/test/java/org/eclipse/collections/companykata/Exercise5Test.java
+++ eclipse-collections-kata/company-kata/src/test/java/org/eclipse/collections/companykata/Exercise5Test.java
@@ -25,13 +25,13 @@ public class Exercise5Test extends CompanyDomainForKata
     @Test
     public void filterOrderValues()
     {
-        List<Order> orders = this.company.getMostRecentCustomer().getOrders();
+        MutableList<Order> orders = this.company.getMostRecentCustomer().getOrders();
         /**
          * Same exercise but don't use static utility - refactor the type of orders and {@link Customer#getOrders()}
          * instead.
          * Get the order values that are greater than 1.5.
          */
-        MutableList<Double> orderValues = null;
+        MutableList<Double> orderValues = orders.collect(Order::getValue);
         MutableList<Double> filtered = orderValues.select(Predicates.greaterThan(1.5));
         Assert.assertEquals(FastList.newListWith(372.5, 1.75), filtered);
         Verify.assertInstanceOf(MutableList.class, this.company.getMostRecentCustomer().getOrders());
@@ -42,13 +42,13 @@ public class Exercise5Test extends CompanyDomainForKata
     @Test
     public void filterOrders()
     {
-        List<Order> orders = this.company.getMostRecentCustomer().getOrders();
+        MutableList<Order> orders = this.company.getMostRecentCustomer().getOrders();
         /**
          * Same exercise but don't use static utility - refactor the type of orders and {@link Customer#getOrders()}
          * instead.
          * Get the actual orders (not their double values) where those orders have a value greater than 2.0.
          */
-        MutableList<Order> filtered = null;
+        MutableList<Order> filtered = orders.select(Predicates.attributeGreaterThan(Order::getValue, 2.0));
         Assert.assertEquals(FastList.newListWith(Iterate.getFirst(this.company.getMostRecentCustomer().getOrders())), filtered);
         Verify.assertInstanceOf(MutableList.class, this.company.getMostRecentCustomer().getOrders());
         this.company.getMostRecentCustomer().getOrders().add(null);
