15c8e842612ebff0670a17e4832b3b032d13482d
shionit <frontier.engine@gmail.com>
2018-10-14 16:36:27 +0900

    add gemini-api

---
+++ spring-boot-vuejs/gemini-api/src/main/java/com/github/shionit/geminiapi/GeminiApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.shionit.geminiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiApiApplication.class, args);
+	}
+}
---
+++ spring-boot-vuejs/gemini-api/src/main/java/com/github/shionit/geminiapi/controller/CurrencyAddRequest.java
@@ -0,0 +1,16 @@
+package com.github.shionit.geminiapi.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by @shionit on 2018/10/09.
+ */
+@Getter
+@Setter
+public class CurrencyAddRequest {
+
+    private String name;
+
+    private String symbol;
+}
