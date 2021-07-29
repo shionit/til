01dd7dd14dc18374e31b04137dbae5603d43047e
shionit <frontier.engine@gmail.com>
2021-07-30 00:51:49 +0900

    Change cookie func

--- go-web-app/ch04/cookie/server.go
+++ go-web-app/ch04/cookie/server.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func setCookie(w http.ResponseWriter, r *http.Request) {
 	c1 := http.Cookie{
@@ -13,8 +16,18 @@ func setCookie(w http.ResponseWriter, r *http.Request) {
 		Value: "Manning Publications Co",
 		HttpOnly: true,
 	}
-	w.Header().Set("Set-Cookie", c1.String())
-	w.Header().Add("Set-Cookie", c2.String())
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request)  {
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+	cs := r.Cookies()
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
 }
 
 func main()  {
@@ -22,5 +35,6 @@ func main()  {
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
 	server.ListenAndServe()
 }
